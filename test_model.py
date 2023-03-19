230575d7254609c8d6f2701a5beeb9b54cce692d
mikemax12 <111198834+mikemax12@users.noreply.github.com>
2023-03-19 00:26:09 -0400

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,6 +1,34 @@
 
 
 
+
+
+
+
+#!pip install transformers
+import transformers
+from transformers import AutoImageProcessor, ViTForImageClassification
+import torch
+import datasets
+from datasets import load_dataset
+
+import time
+import json
+import copy
+import torch
+import numpy as np
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch import nn, optim
+from torch.optim import lr_scheduler
+
+
+from torchvision import transforms
+
+from torchvision import models
+from torchvision import datasets
+
+
 import kaggle
 import os
 from torch.utils.data import Dataset
@@ -12,13 +40,13 @@ class ImageNetKaggle(Dataset):
         self.targets = []
         self.transform = transform
         self.syn_to_class = {}
-        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
+        with open(os.path.join("/shared/local_scratch/gpu1/c_mtmaxwel/ENETER/ILSRVC", "imagenet_class_index.json"), "rb") as f:
                     json_file = json.load(f)
                     for class_id, v in json_file.items():
                         self.syn_to_class[v[0]] = int(class_id)
-        with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
+        with open(os.path.join("/shared/local_scratch/gpu1/c_mtmaxwel/ENETER/ILSRVC", "ILSVRC2012_val_labels.json"), "rb") as f:
                     self.val_to_syn = json.load(f)
-        samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
+        samples_dir = os.path.join("/shared/local_scratch/gpu1/c_mtmaxwel/ENETER/", "ILSVRC/Data/CLS-LOC", split)
         for entry in os.listdir(samples_dir):
             if split == "train":
                 syn_id = entry
@@ -48,7 +76,7 @@ from torchvision import transforms
 import torch
 import torchvision
 from tqdm import tqdm
-model = torchvision.models.resnet50(weights="DEFAULT")
+model = torchvision.models.resnet18()
 model.eval().cuda()  # Needs CUDA, don't bother on CPUs
 mean = (0.485, 0.456, 0.406)
 std = (0.229, 0.224, 0.225)
@@ -69,11 +97,94 @@ dataloader = DataLoader(
             drop_last=False,
             pin_memory=True
         )
-correct = 0
-total = 0
-with torch.no_grad():
-    for x, y in tqdm(dataloader):
-        y_pred = model(x.cuda())
-        correct += (y_pred.argmax(axis=1) == y.cuda()).sum().item()
-        total += len(y)
-print(correct / total)
+
+image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+
+dataset_sizes = len(datasets['val'])
+
+print("start training")
+
+def train_model(model, criteria, optimizer, scheduler,    
+                                      num_epochs=25, device='cuda'):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(1, 2):#num_epochs+1):
+        print('Epoch {}/{}'.format(epoch, num_epochs ))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+            i = 0
+            # Iterate over data.
+            for inputs, labels in dataloader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                i+=1
+                print(i)
+                if (i > 2):
+                  break
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                #print(inputs.shape)
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    print(outputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criteria(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+           
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+criteria = nn.CrossEntropyLoss()
+# Observe that all parameters are being optimized
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+# Number of epochs
+eps=5
+
+model = train_model(model, criteria, optimizer, scheduler, eps, 'cuda')
